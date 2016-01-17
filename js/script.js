3c0d762788b99e1108e987ece75db89c2d879d7b
Simon Yang <simon.z.yang@gmail.com>
2016-01-17 23:06:06 +0800

    Site updated: 2016-01-17 23:06:06

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 (function($){
+  var toTop = $('#toTop').length ? $('#toTop').offset().top - $(window).height() + 20 : 0;
 
   // Share
   $('body').on('click', function(){
@@ -24,10 +25,10 @@
         '<div id="' + id + '" class="article-share-box">',
           '<input class="article-share-input" value="' + url + '">',
           '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="fa fa-twitter article-share-twitter" target="_blank" title="Twitter"></a>',
+            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="fa fa-facebook article-share-facebook" target="_blank" title="Facebook"></a>',
+            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="fa fa-pinterest article-share-pinterest" target="_blank" title="Pinterest"></a>',
+            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="fa fa-google article-share-google" target="_blank" title="Google+"></a>',
           '</div>',
         '</div>'
       ].join('');
@@ -57,29 +58,49 @@
   // Caption
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('image-link')) return;
+      if ($(this).parent().hasClass('fancybox')) return;
 
       var alt = this.alt;
+
       if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="image-link"></a>');
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
     });
 
-    $(this).find('.image-link').each(function(){
+    $(this).find('.fancybox').each(function(){
       $(this).attr('rel', 'article' + i);
     });
   });
 
-  // Bootstrap table style
-  $('.article-entry table').each(function(i, table)  {
-    if ($(this).parent().hasClass('table-responsive')) return;
-    $(this).addClass('table');
-    $(this).wrap('<div class="table-responsive"></div>');
+  if ($.fancybox){
+    $('.fancybox').fancybox();
+  }
+
+  // Profile card
+  $(document).on('click', function () {
+    $('#profile').removeClass('card');
+  }).on('click', '#profile-anchor', function (e) {
+    e.stopPropagation();
+    $('#profile').toggleClass('card');
+  }).on('click', '.profile-inner', function (e) {
+    e.stopPropagation();
   });
 
-  // Lightbox plugin
-  if ($.fancybox){
-    $('.image-link').fancybox();
-  }
+  // To Top
+  $(document).on('scroll', function () {
+    if ($(document).width() >= 800) {
+      if($(this).scrollTop() > toTop) {
+        $('#toTop').addClass('fix');
+        $('#toTop').css('left', $('#sidebar').offset().left);
+      } else {
+        $('#toTop').removeClass('fix');
+      }
+    } else {
+      $('#toTop').addClass('fix');
+      $('#toTop').css('right', 20);
+    }
+  }).on('click', '#toTop', function () {
+    $(document).scrollTop(0);
+  });
 
 })(jQuery);
